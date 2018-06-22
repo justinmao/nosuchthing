980687d69cc69da26d861d46bbb45f68828564dd
Justin Mao <justin.mao@mail.utoronto.ca>
2018-06-21 23:21:36 -0400

    Create desktop header

---
+++ header.js
@@ -0,0 +1,25 @@
+(function () {
+  let desktopHeader = document.getElementById('header-desktop');
+  // Spacer
+  let headspace = document.getElementById('headspace');
+  headspace.style.height = desktopHeader.clientHeight + "px";
+  // Shadow listener
+  document.addEventListener('scroll', () => {
+    y = window.pageYOffset;
+    if (y > 150) {
+      desktopHeader.classList.add('lift');
+    } else {
+      desktopHeader.classList.remove('lift');
+    }
+    // Hide submenus
+    let submenus = document.getElementsByClassName('submenu');
+    for (submenu of submenus) {
+      submenu.classList.remove('active');
+    }
+  });
+  // Header submenus
+  let services = document.getElementById('link-services');
+  services.addEventListener('click', () => {
+    document.getElementById('submenu-services').classList.add('active');
+  });
+})();
